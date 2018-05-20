9b6db7a0db5c6b805a807a69c0f5d8f95f9ed7f5
EarlyH <earlyhe0@gmail.com>
2018-05-20 20:47:08 +0800

    fix lint

--- test/e2e/components/link.js
+++ test/e2e/components/link.js
@@ -32,10 +32,10 @@ test.serial('basic style', async t => {
     cleanstyle(result.style);
 
     // cause : circleci use other webkit.
-delete result.style.inlineSize;
-delete result.style.perspectiveOrigin;
-delete result.style.webkitLogicalWidth;
-delete result.style.width;
+    delete result.style.inlineSize;
+    delete result.style.perspectiveOrigin;
+    delete result.style.webkitLogicalWidth;
+    delete result.style.width;
 
     t.snapshot(result);
 
