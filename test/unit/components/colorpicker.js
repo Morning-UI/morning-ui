7937e83d84dec82ed6e2dfa4ade506a0d6e64c1b
EarlyH <earlyhe0@gmail.com>
2018-06-03 10:39:17 +0800

    fix coverage

--- test/unit/components/colorpicker.js
+++ test/unit/components/colorpicker.js
@@ -12,6 +12,9 @@ test.serial('base : component snapshot', async t => {
 
     t.plan(1);
 
+    // cause : slider's tip-formatter is a function, when run coverage test, the function always different.
+    delete vm.$el;
+
     snapshot(t, vm);
 
 });
