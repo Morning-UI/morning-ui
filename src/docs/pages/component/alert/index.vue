82000e474ee8daaac04e3ba55c5574bb9063c411
EarlyH <earlyhe0@gmail.com>
2018-07-26 12:43:56 +0800

    save work

---
+++ src/docs/pages/component/alert/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <doc-component
+        :page="page"
+        :hasPadding="true"
+    >
+    <script type="text/markdown">
+    # 告示 `<ui-alert>`
+    
+    <doc-component-status page="alert"></doc-component-status>
+    
+    [[[开始]]]
+
+    定义一个告示。
+
+    #### 使用
+
+    :::democode/html
+    <ui-alert>展示的内容</ui-alert>
+    :::
+
+    [[[源码]]]
+
+    <iframe src="/report/coverage/lib/components/alert/index.vue.html" name="codeFrame" frameborder="0" onload="this.height=codeFrame.document.body.scrollHeight"></iframe>
+
+    </script>
+    </doc-component>
+</template>
+ 
+<script>
+import DocComponent                 from 'Docs/common/DocComponent.vue';
+
+export default {
+    data : function () {
+
+        return {
+            page : 'alert'
+        };
+
+    },
+    components : {
+        'doc-component' : DocComponent
+    }
+};
+</script>
+
+<style lang="less">
+</style>
---
+++ src/lib/components/alert/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <mor-alert
+        :_uiid="uiid"
+        :class="[]"
+    >
+        
+    </mor-alert>
+</template>
+ 
+<script>
+export default {
+    origin : 'UI',
+    name : 'alert',
+    props : {},
+    computed : {
+        _conf : function () {
+
+            return {};
+
+        }
+    },
+    data : function () {
+
+        return {
+            data : {}
+        };
+
+    },
+    methods : {},
+    mounted : function () {}
+};
+</script>
+
+<style lang="less" src="./index.less"></style>
