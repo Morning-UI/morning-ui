3fe620fe9eb094b59f6b875dac035e6cb5d1cd7d
EarlyH <earlyhe0@gmail.com>
2018-08-24 09:56:32 +0800

    update

--- src/lib/common/ui.js
+++ src/lib/common/ui.js
@@ -164,7 +164,7 @@ export default (Vue, morning) => Vue.extend({
         this.$watch('_conf', val => {
 
             this.conf = Object.assign({}, this.conf, val);
-            
+
         }, {
             immediate : true,
             deep : true
