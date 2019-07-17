e82fd6aeae3972c09b62e4cba444eabd58ada918
EarlyH <earlyhe0@gmail.com>
2019-07-17 16:28:46 +0800

    rm some components

--- src/docs/pages/component/textright/index.js
+++
@@ -1,7 +0,0 @@
-import Vue                          from 'vue';
-import Index                        from './index.vue';
-
-new Vue({
-    el : '#root',
-    render : h => h(Index)
-});
--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,17 +1,6 @@
 import h                            from './h/index.vue';
 import lead                         from './lead/index.vue';
 import mark                         from './mark/index.vue';
-import del                          from './del/index.vue';
-import u                            from './u/index.vue';
-import small                        from './small/index.vue';
-import strong                       from './strong/index.vue';
-import em                           from './em/index.vue';
-import textcenter                   from './textcenter/index.vue';
-import textleft                     from './textleft/index.vue';
-import textright                    from './textright/index.vue';
-import lowercase                    from './lowercase/index.vue';
-import uppercase                    from './uppercase/index.vue';
-import capitalize                   from './capitalize/index.vue';
 import quote                        from './quote/index.vue';
 import ol                           from './ol/index.vue';
 import ul                           from './ul/index.vue';
@@ -106,17 +95,6 @@ export default {
     h,
     lead,
     mark,
-    del,
-    u,
-    small,
-    strong,
-    em,
-    textcenter,
-    textleft,
-    textright,
-    lowercase,
-    uppercase,
-    capitalize,
     quote,
     ol,
     ul,
