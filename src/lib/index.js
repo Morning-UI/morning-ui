abeedf0447049039e070c974eb250155c0f9cc7e
EarlyH <earlyhe0@gmail.com>
2019-11-04 09:36:06 +0800

    update version info for 0.12.65

--- src/lib/index.js
+++ src/lib/index.js
@@ -48,9 +48,9 @@ let morning = {
         black : 'bk',
         white : 'wh'
     },
-    _devVersion : '0.12.64',
+    _devVersion : '0.12.65',
     isMorning : true,
-    version : '0.12.64',
+    version : '0.12.65',
     map : {}
 };
 
