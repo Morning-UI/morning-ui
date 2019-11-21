87fe83b4d6417a1de502a7dcf323b4eb6c8b01c6
EarlyH <earlyhe0@gmail.com>
2019-11-21 20:23:10 +0800

    update version info for 0.12.71

--- src/lib/index.js
+++ src/lib/index.js
@@ -49,9 +49,9 @@ let morning = {
         white : 'wh'
     },
     _mindmapClipboard : undefined,
-    _devVersion : '0.12.70',
+    _devVersion : '0.12.71',
     isMorning : true,
-    version : '0.12.70',
+    version : '0.12.71',
     map : {}
 };
 
